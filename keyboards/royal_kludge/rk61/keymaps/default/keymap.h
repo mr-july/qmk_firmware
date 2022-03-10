0e74521428d11614da68d56d1dfe8029900061e0
mr. JULY <mr_july@mail.ru>
2022-03-10 16:20:02 +0100

    introduce support for Royal Kludge RK61 non-hotswap rgb edition (PID 0x24F)
    This is improved version of euwbah (see https://github.com/euwbah/qmk_firmware/tree/sn32/keyboards/royal_kludge/rk61_rgb):
    - better organized directory structure
    - add support for ISO layout
    - implement a keymap, which supports Neo Layout (https://www.neo-layout.org/) layers 3 and 4

---
+++ keyboards/royal_kludge/rk61/keymaps/default/keymap.h
@@ -0,0 +1,50 @@
+/*
+Copyright 2022 mr. JULY <mr_july@mail.ru>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Row:     0           1           2           3           4           5           6           7           8           9           10          11          12          13          */
+
+// default layer
+#define BASE_KEYMAP_(caps, nuhs, nubs) { \
+        {   KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    }, \
+        {   KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    }, \
+        {   caps,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    nuhs,       KC_ENT,     }, \
+        {   KC_LSFT,    nubs,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_UP,      KC_RSFT,    XXXXXXX,    }, \
+        {   KC_LCTL,    KC_LGUI,    KC_LALT,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_SPC,     XXXXXXX,    XXXXXXX,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   XXXXXXX,    MO(_FN),    }  \
+    }
+#define BASE_KEYMAP BASE_KEYMAP_(KC_CAPS, KC_NUHS, KC_NUBS)
+
+// snap right hand modifiers: Alt, context menu, Ctrl instead of arrows
+#define RMODS_KEYMAP_(ralt) { \
+        {   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    }, \
+        {   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    }, \
+        {   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    }, \
+        {   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_SLSH,    _______,    _______,    }, \
+        {   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    ralt,       KC_APP,     KC_RCTL,    _______,    _______,    }  \
+    }
+
+#define RMODS_KEYMAP RMODS_KEYMAP_(KC_RALT)
+
+// functional layer (F1-F10, backlight controls etc.)
+#define FN_KEYMAP { \
+        {   KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,     }, \
+        {   KC_TRNS,    KC_F13,     KC_F14,     KC_F15,     KC_F16,     KC_F17,     KC_PSCR,    KC_SLCK,    KC_PAUS,    KC_TRNS,    RGB_HUI,    RGB_SAI,    RGB_VAI,    RGB_MOD,    }, \
+        {   RESET,      KC_VOLU,    KC_BRIU,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_INS,     KC_HOME,    KC_PGUP,    KC_TRNS,    RGB_SPD,    RGB_SPI,    RGB_MOD,    TG(_RMODS), }, \
+        {   _______,    _______,    KC_VOLD,    KC_BRID,    _______,    _______,    _______,    KC_DEL,     KC_END,     KC_PGDN,    _______,    KC_SLSH,    _______,    _______,    }, \
+        {   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_RALT,    KC_APP,     KC_RCTL,    _______,    _______,    }  \
+    }
---
+++ keyboards/royal_kludge/rk61/rgb/readme.md
@@ -0,0 +1,57 @@
+# RK61 RGB
+
+![RK61 RGB](https://i.pinimg.com/originals/01/e2/1f/01e21f1ba56526a0f2fe5047b6d25f11.png)
+
+Royal Kludge RK61 non-hotswap rgb edition, original PID 0x24F.
+
+Tested on the Royal Kludge RK61 non-hotswap RGB dual-mode edition, original PID 0x24F
+(RK61 firmware v1.03.000n, purchased after May 2020).
+
+- MCU: HFD2201KBA (rebrand SN32F248BF)
+- PCB: RK61-JX-RGB-HFD; REV: 01; 2019/08/13
+- Wired only, no bluetooth support.
+
+Keyboard Maintainer: [euwbah](https://github.com/euwbah)
+
+For the white-backlit non-rgb version of the RK61, check out [Diff-fusion's repo](https://github.com/Diff-fusion/qmk_firmware)
+
+
+Note: keymap differs slightly from stock firmware for FN combinations
+configuring RGB settings. Also adds ability to control volume and display
+brightness + additional FN keys.
+See [keymap.c](keymaps/default/keymap.c), and refer to the [list
+of QMK keycodes](https://beta.docs.qmk.fm/using-qmk/simple-keycodes/keycodes).
+
+-----------------
+
+
+Make example for this keyboard (after setting up your build environment):
+
+    make royal_kludge/rk61_rgb:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+## Bootloader
+
+Enter the bootloader:
+
+- Physical reset: short the two contacts to the left of the space bar switch with a wire/paperclip, accessible from the bottom of PCB.
+  ![HW bootloader](imgs/enter-bootloader.jpg)
+  After shorting, just connect the USB cable. The keyboard started in bootloader mode and is ready to be flashed.
+- Layout (after loading QMK): FN + CapsLock
+
+## Flashing
+
+### Option 1. SONiX USB MCU ISP Tool
+
+Download the [SONiX USB MCU ISP Tool](https://www.sonix.com.tw/files/1/B6648B8BB74030BDE050007F01002479) from the [MCU SN32F248B page](https://www.sonix.com.tw/article-en-4336-30356) and use the program, included in archive.
+More detailed instructions may be found in this [Video](https://youtu.be/2MHtLEiXaXg?t=940).
+
+### Option 2. Sonix Flasher
+
+Use the Python program from [this repository](https://github.com/SonixQMK/sonix-flasher)
+
+### Option 3. SonixFlasherC
+
+Use the Python program from [this repository](https://github.com/SonixQMK/SonixFlasherC)
+
