c1cbd580ac5752e28377b793f1a4427f42c584d6
Igor Lubimov <mr_july@mail.ru>
2022-03-14 10:44:56 +0100

    add missed "0" to the Neo layer 4

--- keyboards/royal_kludge/rk61/keymaps/iso-neo-l3l4/keymap.c
+++ keyboards/royal_kludge/rk61/keymaps/iso-neo-l3l4/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {   _______,    KC_PGUP,    KC_BSPC,    KC_UP,      KC_DEL,     KC_PGDN,    KC_PSLS,    KC_P7,      KC_P8,      KC_P9,      KC_PMNS,    _______,    _______,    _______,    },
         {   _______,    KC_HOME,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_END,     KC_PAST,    KC_P4,      KC_P5,      KC_P6,      KC_PPLS,    _______,    _______,    _______,    },
         {   _______,    _______,    KC_ESC,     KC_TAB,     KC_INS,     KC_ENTER,   LCTL(DE_Z), KC_NLCK,    KC_P1,      KC_P2,      KC_P3,      KC_PDOT,    _______,    _______,    },
-        {   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    } 
+        {   _______,    _______,    _______,    _______,    _______,    _______,    KC_P0,      _______,    _______,    _______,    _______,    _______,    _______,    _______,    } 
     },
     [_FN] = FN_KEYMAP
 };
