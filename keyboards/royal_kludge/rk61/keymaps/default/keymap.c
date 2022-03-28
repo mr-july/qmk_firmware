a65a8d8d350969c72ec299484f13954f23b14c28
mr. JULY <mr_july@mail.ru>
2022-03-28 20:32:09 +0200

    rename "keymap.h" to "layout.h" to prevent compile problems

--- keyboards/royal_kludge/rk61/keymaps/default/keymap.c
+++ keyboards/royal_kludge/rk61/keymaps/default/keymap.c
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-#include "keymap.h"
+#include "layout.h"
 
 enum layer_names {
     _BASE,   // default layer
