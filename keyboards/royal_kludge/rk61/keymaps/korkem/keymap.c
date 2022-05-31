65b6616f422733760f5f40a02148e430ff720f3e
Igor Lubimov <mr_july@mail.ru>
2022-05-31 17:42:28 +0200

    add mouse buttons to mouse layer

--- keyboards/royal_kludge/rk61/keymaps/korkem/keymap.c
+++ keyboards/royal_kludge/rk61/keymaps/korkem/keymap.c
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_WH_U,    KC_WH_U,    KC_MS_U,    KC_WH_D,    KC_WH_D,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RESET,
     KC_WH_L,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_WH_R,    XXXXXXX,    KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,
     U_UND,      U_CUT,      U_CPY,      U_PST,      U_RDO,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-                            KC_TAB,     KC_ESC,           KC_SPC,           KC_BSPC,    XXXXXXX
+                            KC_BTN2,    KC_BTN3,          KC_BTN1,          KC_BSPC,    XXXXXXX
   ),
   [_NUM] = LAYOUT_korkem(
     U_UND,      U_CUT,      U_CPY,      U_PST,      U_RDO,      KC_PSLS,    KC_P7,      KC_P8,      KC_P9,      KC_PMNS,
