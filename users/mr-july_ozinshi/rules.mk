b00ae2277aa0e87cbafb8b24f6500dc3a98a7825
mr. JULY <mr_july@mail.ru>
2022-11-26 18:47:38 +0100

    introduce Ozinshi layout for Planck keyboard

---
+++ users/mr-july_ozinshi/rules.mk
@@ -0,0 +1,24 @@
+# Copyright 2022 mr. JULY <mr_july@mail.ru>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+KEY_OVERRIDE_ENABLE = yes   # Enable send different key codes depending on the modifiers
+
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+
+COMMAND_ENABLE = no         # Enable the way to change keyboard’s behavior without having to flash or unplug it
+CAPS_WORD_ENABLE = yes      # Enable the CAPS_WORD support
+
+INTROSPECTION_KEYMAP_C = mr-july_ozinshi.c # keymaps
