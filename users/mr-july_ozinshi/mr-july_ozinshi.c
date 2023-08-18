991234337cb1090bcb28834708666757249a74e7
mr. JULY <mr_july@mail.ru>
2023-08-19 00:00:12 +0200

    fix: correct some key definitions (key code names), because they was renamed in quantum

--- users/mr-july_ozinshi/mr-july_ozinshi.c
+++ users/mr-july_ozinshi/mr-july_ozinshi.c
@@ -158,10 +158,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PGUP,    KC_BSPC,    KC_UP,      KC_DEL,     KC_PGDN,    U_RDO,      U_PST,      U_CPY,      U_CUT,      U_UND,
     KC_HOME,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_END,     XXXXXXX,    KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,
     KC_CAPS,    KC_TAB,     KC_INS,     KC_ENT,     KC_ENT,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-                            CAPSWRD,                     KC_SPC,                        XXXXXXX
+                            CW_TOGG,                     KC_SPC,                        XXXXXXX
   ),
   [_MOU]    = LAYOUT_ozinshi(
-    KC_WH_U,    KC_WH_U,    KC_MS_U,    KC_WH_D,    KC_WH_D,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RESET,
+    KC_WH_U,    KC_WH_U,    KC_MS_U,    KC_WH_D,    KC_WH_D,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    QK_RBT,
     KC_WH_L,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_WH_R,    XXXXXXX,    KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,
     U_UND,      U_CUT,      U_CPY,      U_PST,      U_RDO,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
                             KC_BTN2,                     KC_BTN1,                       KC_BTN3
@@ -169,12 +169,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NUM]    = LAYOUT_ozinshi(
     U_UND,      U_CUT,      U_CPY,      U_PST,      U_RDO,      KC_PSLS,    KC_P7,      KC_P8,      KC_P9,      KC_PMNS,
     KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_PAST,    KC_P4,      KC_P5,      KC_P6,      KC_PPLS,
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_ENT,     XXXXXXX,    KC_NLCK,    KC_P1,      KC_P2,      KC_P3,      KC_PDOT,
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_ENT,     XXXXXXX,    KC_NUM,     KC_P1,      KC_P2,      KC_P3,      KC_PDOT,
                             XXXXXXX,                     KC_SPC,                        KC_P0
   ),
   [_FUN]    = LAYOUT_ozinshi(
     U_UND,      U_CUT,      U_CPY,      U_PST,      U_RDO,      KC_PSCR,    KC_F7,      KC_F8,      KC_F9,      KC_F12,
-    KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_SLCK,    KC_F4,      KC_F5,      KC_F6,      KC_F11,
+    KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_LSCR,    KC_F4,      KC_F5,      KC_F6,      KC_F11,
     XXXXXXX,    DE_BASE,    EN_BASE,    DW_BASE,    EW_BASE,    KC_PAUS,    KC_F1,      KC_F2,      KC_F3,      KC_F10,
                             KC_TAB,                      KC_SPC,                        KC_BSPC
   ),
@@ -223,7 +223,7 @@ bool process_custom_mod_tap(uint16_t keycode, keyrecord_t* record) {
                     : IS_GERMAN ? DE_QUES : KC_QUES;
 
                 tap_code16(code);
-           
+
                 return false;  // Return false to ignore further processing of key
             }
     }
